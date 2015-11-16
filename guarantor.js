5a6503c9747d6315790b1ba5f546660a4becc32f
Erik Pukinskis <erik@snowedin.net>
2015-11-15 22:38:55 -0800

    Stop trying stuff if we exit multiple times

--- guarantor.js
+++ guarantor.js
@@ -10,16 +10,19 @@ function guarantor(cleaner) {
 }
 
 function cleanUpOnExit() {
-
   process.on('exit', function() {
+    if (cleaning) { return }
     cleanUp(0)
-
     var remaining = oneRemaining()
 
     if (remaining) {
-      var nice = remaining.toString().replace(/[\n\s]+/g, " ")
+      var source = remaining.toString().replace(/[\n\s]+/g, " ")
 
-      console.log("\nYou wanted to guarantee that "+nice+" ran, but it seems to be doing something asynchronous and Node is shutting down NOW! We tried our best!")
+      if (source.length > 51) {
+        source = source.slice(0,50)+"..."
+      }
+
+      console.log("\nYou wanted to guarantee that \""+source+"\" ran, but it seems to be doing something asynchronous and Node is shutting down NOW! We tried our best!")
     }
   })
 
@@ -41,7 +44,12 @@ function cleanUpOnExit() {
 
 }
 
+var cleaning = false
+
 function cleanUp(status, callback) {
+  if (cleaning) { return }
+  else { cleaning = true }
+
   function markFinished(index) {
     returned[index] = true
     if (!oneRemaining()) {
